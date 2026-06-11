baa9cf08b7efe84311d1ffb56cd21a58fb6ba7cd
SG HARSHAA <harshaavardhan8@gmail.com>
2026-06-11 17:11:02 +0530

    macbook-m4overview

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlight from './components/Highlight'
 import Showcase from './components/Showcase'
 import Footer from './components/Footer'
-
 import Lenis from 'lenis'
 
 const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
 
-const lenis = new Lenis();
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
 
-// Use requestAnimationFrame to continuously update the scroll
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
+    const rafId = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(rafId);
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <div>
       <Navbar/>
       <Hero/>
       <Highlight/>
       <Showcase/>
-      <Footer/> 
+      <Footer/>
     </div>
   )
 }
